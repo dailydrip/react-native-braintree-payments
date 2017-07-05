81cc4e7b3cb84c47bc19027ba8a8c86272a47979
Franzé Jr <franzejr@gmail.com>
2017-07-04 22:06:05 -0300

    Creating routes file and using it in our index files for android and ios

--- index.android.js
+++ index.android.js
@@ -1,4 +1,7 @@
 import { AppRegistry } from "react-native";
-import App from "./src/App";
+import { DrawerNavigator } from "react-navigation";
+import Routes from "./routes";
 
-AppRegistry.registerComponent("dailydripStarterApp", () => App);
+const StarterApp = DrawerNavigator(Routes);
+
+AppRegistry.registerComponent("dailydripStarterApp", () => StarterApp);
---
+++ routes.js
@@ -0,0 +1,7 @@
+import App from "./src/App";
+import SecondScreen from "./src/SecondScreen";
+
+export default {
+  Home: { screen: App },
+  SecondScreen: { screen: SecondScreen }
+};
